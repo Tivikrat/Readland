646ec590594072424bd5110a7e50d7b953dc748c
Vadym Karachenko <1916854+VidroX@users.noreply.github.com>
2019-10-22 18:49:45 +0300

    Update settings.py

--- Backend/readland/readland/settings.py
+++ Backend/readland/readland/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bns)0hbr31)4hb5@cig^=ufa*8e%j+7v5(7z)^0xb%oc2zx=w8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
