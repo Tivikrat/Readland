542bf30a5dc7936a761a366b748271111593dbe2
Marko <marko.andrushchenko@nure.ua>
2019-10-23 22:26:10 +0300

    footer to bottom

---
+++ Front/assets/js/script.js
@@ -0,0 +1,10 @@
+/*document.onload = function () {
+    alert(this)
+}
+*/window.onload = function () {
+    this.alert(window.screen.height)
+    d = this.document.getElementById("footer")
+    d.style.position= 'relative'
+    d.style.top = window.screen.height + 'px';
+};
+   
