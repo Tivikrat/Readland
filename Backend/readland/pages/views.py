451607961f8187838f600f337bcbfa8d96ce06f9
Marko <marko.andrushchenko@nure.ua>
2019-11-09 00:46:24 +0200

    added beter tag split

--- Backend/readland/pages/views.py
+++ Backend/readland/pages/views.py
@@ -46,7 +46,7 @@ def read_book(request, book_id):
 
 def view_book_info(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
-    tag = book.tag.split(",")
+    tag = book.tag.split(" ")
     return render(request, 'bookoverview.html', {"name": book.name,
                                                  "tag": tag,
                                                  "date": book.date,
