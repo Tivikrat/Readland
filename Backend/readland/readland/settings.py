c8265366634e01e4c7b4d0593798e1864e11bd14
Yaroslav Honchar <yaroslav.honchar@nure.ua>
2019-12-24 21:50:29 +0200

    Update settings.py

--- Backend/readland/readland/settings.py
+++ Backend/readland/readland/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'uk-UA'
+LANGUAGE_CODE = 'uk'
 
 TIME_ZONE = 'Europe/Kiev'
 
