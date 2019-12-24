7cf0f6248f772c42a7b805fe6120454ac539cfaf
Marko <marko.andrushchenko@nure.ua>
2019-12-24 16:32:33 +0200

    added pages
    +book_lists page
    +change_book_info page page
    +register page
    ~bootstrapStudioFiles

--- Backend/readland/readland/settings.py
+++ Backend/readland/readland/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'readland',
         'USER': 'readland',
-        'PASSWORD': '123321',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
